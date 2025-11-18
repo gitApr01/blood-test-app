fdd5f23a5583671491c423145397f0db5a423a7e
gitApr01 <aprhomeo@gmail.com>
2025-11-18 20:43:37 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,7 +8,7 @@ function Login() {
   const [password, setPassword] = useState("");
 
   const loginUser = async () => {
-    const res = await fetch("http://localhost:5000/login", {
+    const res = await fetch("https://blood-test-app.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
