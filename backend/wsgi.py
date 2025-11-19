7953a7a18ff48ed05096bd112a2557d28757b74d
gitApr01 <aprhomeo@gmail.com>
2025-11-19 11:22:40 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,22 +1,25 @@
 from app import create_app, db
 from app.models import User
+from werkzeug.security import generate_password_hash
 from sqlalchemy import select
 
 app = create_app()
 
-# Ensure database and admin user exist
 with app.app_context():
-
     # Create tables if missing
     db.create_all()
 
-    # Check if admin exists
+    # Check for admin
     existing = db.session.execute(
         select(User).where(User.username == "admin")
     ).first()
 
     # Create admin if missing
     if not existing:
-        admin = User(username="admin", password="admin123", role="admin")
+        admin = User(
+            username="admin",
+            password=generate_password_hash("admin123"),
+            role="admin"
+        )
         db.session.add(admin)
         db.session.commit()
