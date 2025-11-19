02cf257df6d59b2866d1640f14b29cea2c847e63
gitApr01 <aprhomeo@gmail.com>
2025-11-19 14:28:51 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,47 +1,28 @@
 const path = require("path");
 
 module.exports = {
-  context: path.resolve(__dirname),
   entry: "./src/index.js",
-
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
 
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ]
-          }
-        }
+        use: "babel-loader",
       }
-    ]
+    ],
   },
 
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".js", ".jsx"],
   },
 
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-
-  mode: "production"
 };
