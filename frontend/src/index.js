b3be5b0e7b89509e38d521eb5e18a37589db357b
gitApr01 <aprhomeo@gmail.com>
2025-11-19 14:26:47 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
-// frontend/src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
