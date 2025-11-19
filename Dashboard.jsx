c0095d2844f3eebf875106a74cded4315cd6e691
gitApr01 <aprhomeo@gmail.com>
2025-11-19 21:44:52 +0530

    Add files via upload

---
+++ Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../components/Header.jsx";
+export default function Dashboard(){
+  return <>
+    <Header />
+    <div className='card'><h2>Dashboard</h2><p>Welcome</p></div>
+  </>;
+}
