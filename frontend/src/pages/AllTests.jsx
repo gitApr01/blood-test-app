b242a3bbdc4f6f722af4883268468af4582c8d43
gitApr01 <aprhomeo@gmail.com>
2025-11-20 20:46:08 +0530

    Update AllTests.jsx

--- frontend/src/pages/AllTests.jsx
+++ frontend/src/pages/AllTests.jsx
@@ -5,7 +5,7 @@ import Loader from "../components/Loader";
 
 export default function AllTests(){
   const [data,setData]=useState([]); const [loading,setLoading]=useState(false);
-  async function load(){ setLoading(true); try { const r = await fetch("https://blood-test-api.onrender.com/all_tests"); const j=await r.json(); setData(j);} catch(e){console.error(e);} setLoading(false); }
+  async function load(){ setLoading(true); try { const r = await fetch("https://blood-test-app.onrender.com/all_tests"); const j=await r.json(); setData(j);} catch(e){console.error(e);} setLoading(false); }
   useEffect(()=>{ load(); },[]);
   return <>
     <Header title="All Tests"/>
