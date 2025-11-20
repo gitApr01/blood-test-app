77358bb43f11fbedd98765c546b69b082ba14d8f
gitApr01 <aprhomeo@gmail.com>
2025-11-20 13:11:41 +0530

    Delete frontend directory

--- frontend/src/pages/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "../components/Header.jsx";
-export default function Dashboard(){
-  return <>
-    <Header />
-    <div className='card'><h2>Dashboard</h2><p>Welcome</p></div>
-  </>;
-}
